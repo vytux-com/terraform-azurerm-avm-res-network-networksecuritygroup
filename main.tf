eb98746ffaffd6f9a8767afe7198af47f7bcbabc
Vytautas Krivickas <vytautas.krivickas@steadfasttech.com.au>
2025-11-24 19:58:00 +1100

    Try tomap

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "azurerm_network_security_group" "this" {
   tags                = var.tags
 
   dynamic "security_rule" {
-    for_each = var.enable_inline_rules ? [var.security_rules] : [""]
+    for_each = var.enable_inline_rules ? [var.security_rules] : [tomap("")]
 
     content {
       access                                     = length(security_rule.value) == 0 ? null : security_rule.value.access
